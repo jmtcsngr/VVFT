6a3f76b7556a6d59ca6782f26caeddebeb20a29b
jmtcsngr <jmtc@sanger.ac.uk>
2016-09-21 18:11:03 +0100

    with before script

--- .travis/before_script.sh
+++ .travis/before_script.sh
@@ -7,10 +7,18 @@
 
 set -e -x
 
-if [ $TRAVIS_BRANCH -eq "devel" ]; then
-  if [ $TRAVIS_REPO_SLUG -eq "jmtcsngr/VVFT" ]; then
+if [ $TRAVIS_REPO_SLUG -eq "jmtcsngr/VVFT" ]; then
+  if [ $TRAVIS_BRANCH -eq "devel" ] || [$TRAVIS_BRANCH -eq "master"]; then
     pushd /tmp
-      
+    for project in "ml_warehouse" "npg_tracking"
+    do
+      pushd $project
+      branch_name = $(git rev-parse --abbrev-ref HEAD)
+      if [ $branch_name -ne $TRAVIS_BRANCH ]; then
+        return 1;
+      fi
+
+    done
     popd
   fi
 fi
@@ -22,5 +30,3 @@ git clone --branch devel --depth 1 https://github.com/wtsi-npg/ml_warehouse.git 
 git clone --branch lims4composition --depth 1 https://github.com/mgcam/npg_tracking.git npg_tracking.git
 git clone --branch devel --depth 1 https://github.com/wtsi-npg/npg_seq_common.git npg_seq_common.git
 
-echo $TRAVIS_BRANCH
-echo $TRAVIS_REPO_SLUG
